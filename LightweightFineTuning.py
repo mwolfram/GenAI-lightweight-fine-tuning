6f62fc024e6dc173b83f48adcf94836bbb598012
Mate Wolfram <mate.wolfram@gmail.com>
2024-08-05 15:47:48 +0000

    complete procedure

--- LightweightFineTuning.py
+++ LightweightFineTuning.py
@@ -10,10 +10,6 @@ import random
 import numpy as np
 import torch
 
-random.seed(42)
-np.random.seed(42)
-torch.manual_seed(42)
-
 # tokenizer
 TOKENIZER_NAME = "facebook/opt-350m"
 #TOKENIZER_NAME = "distilbert-base-uncased"
@@ -25,6 +21,10 @@ MODEL_NAME = TOKENIZER_NAME
 # downsize the data
 FRACTION = 1.0
 
+def reset_random_seed():
+    random.seed(42)
+    np.random.seed(42)
+    torch.manual_seed(42)
 
 def train():
     data = sms_spam().reduce_to_fraction(FRACTION)
@@ -33,28 +33,30 @@ def train():
 
     tokenizer_wrapper.tokenize(data)
     
-    # Uncomment blocks individually:
-
     # Evaluate initial model with HF
-    #trainer_wrapper.init_trainer(data, tokenizer_wrapper.get_tokenizer(), lora=False)
-    #trainer_wrapper.evaluate()
+    trainer_wrapper.init_trainer(data, tokenizer_wrapper.get_tokenizer(), lora=False)
+    trainer_wrapper.evaluate()
 
     # Evaluate with own metrics
+    reset_random_seed()
     trainer_wrapper.evaluate_with_own_loop(data, trainer_wrapper.get_model(data))
 
     # Train Lora model
-    #trainer_wrapper.train_with_own_loop(data, lora=True)
+    reset_random_seed()
+    trainer_wrapper.train_with_own_loop(data, lora=True)
 
     # Evaluate saved Lora model
-    #model_path = "mwolfram/facebook/opt-350m-lora"
-    #model = trainer_wrapper.load_peft_model(model_path)
-    #trainer_wrapper.evaluate_with_own_loop(data, model)
+    reset_random_seed()
+    model_path = "mwolfram/facebook/opt-350m-lora"
+    model = trainer_wrapper.load_peft_model(model_path)
+    trainer_wrapper.evaluate_with_own_loop(data, model)
 
     # Generate text from saved model
     #trainer_wrapper.generate_from_saved_model(tokenizer_wrapper.get_tokenizer(), model_path)
 
 
 if __name__ == "__main__":
+    reset_random_seed()
     enable_cuda()
     check_versions()
     train()
